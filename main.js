c1e1251a45c82bc9d7fe1c7917d332459998ff13
Tomasz Czarnecki <creo.one@gmail.com>
2017-12-03 18:00:24 +0100

    Introducing device motion and orientation drawing on canvas.
    After long struggle with mobile inconsistencies.

--- main.js
+++ main.js
@@ -1,17 +1,56 @@
 
-"use strict";
+var canvas = document.getElementsByTagName("canvas")[0];
+var context = canvas.getContext("2d");
 
-
-var deviceOrientationHandler = function(o, e)
+var resizeHandler = function(e)
 {
-    console.log("device orientation", e);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", resizeHandler, false);
+
+var acc = [100, 0, 0];
+var gyr = [0, 0, 0];
+
+var deviceOrientationHandler = function(e)
+{
+    if(!e) return;
+
+    gyr = [e.alpha, e.beta, e.gamma];
 };
 
 window.addEventListener("deviceorientation", deviceOrientationHandler, false);
 
-var deviceMotionHandler = function(o, e)
+var deviceMotionHandler = function(e)
 {
-    console.log("device motion", e);
+    if(!e) return;
+
+    acc = [e.acceleration.x, e.acceleration.y, e.acceleration.z];
 };
 
 window.addEventListener("devicemotion", deviceMotionHandler, false);
+
+
+var index = 0;
+
+var redraw = function()
+{
+    var size = [canvas.clientWidth, canvas.clientHeight];
+    var middle = [size[0] / 2.0, size[1] / 2.0];
+    
+    context.fillStyle = "black";
+    context.fillRect(0, 0, size[0], size[1]);
+
+    index += 0.01;
+    context.strokeStyle = "white";
+    context.lineWidth = 5;
+    context.beginPath();
+    context.moveTo(middle[0], middle[1]);
+    context.lineTo(middle[0] + gyr[0] * 100, middle[1] + gyr[1] * 100);
+    context.closePath();
+    context.stroke();
+};
+
+var redrawHandle = window.setInterval(redraw, 100);
+resizeHandler(this);
