f073063944964d26369dd9faba398bc2034db364
Tomasz Czarnecki <creo.one@gmail.com>
2017-12-05 20:21:04 +0100

    Added second canvas and started some gyro normalizations.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
-var canvas = document.getElementsByTagName("canvas")[0];
-var context = canvas.getContext("2d");
+var canvasLeft = document.getElementById("canvasLeft");
+var contextLeft = canvasLeft.getContext("2d");
+
+var canvasRight = document.getElementById("canvasRight");
+var contextRight = canvasRight.getContext("2d");
 
 var canvasDataHolder = function()
 {
@@ -10,11 +13,14 @@ var canvasDataHolder = function()
 
 var resizeHandler = function(e)
 {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvasDataHolder.size = [Math.floor(window.screen.width / 2), window.screen.height];
+    canvasDataHolder.middle = [canvasDataHolder.size[0] / 2, canvasDataHolder.size[1] / 2];
 
-    canvasDataHolder.size = [canvas.clientWidth, canvas.clientHeight];
-    canvasDataHolder.middle = [canvasDataHolder.size[0] / 2.0, canvasDataHolder.size[1] / 2.0];
+    canvasLeft.width = canvasDataHolder.size[0];
+    canvasLeft.height = canvasDataHolder.size[1];
+
+    canvasRight.width = canvasDataHolder.size[0];
+    canvasRight.height = canvasDataHolder.size[1];
 }
 
 window.addEventListener("resize", resizeHandler, false);
@@ -27,7 +33,8 @@ var spatialDataHolder = function()
 
 var deviceOrientationHandler = function(e)
 {
-    spatialDataHolder.orientation = [e.alpha, e.beta, e.gamma];
+    var alpha = e.alpha > 180 ? -360 + e.alpha : e.alpha;
+    spatialDataHolder.orientation = [alpha, e.beta, e.gamma];
 };
 
 window.addEventListener("deviceorientation", deviceOrientationHandler, false);
@@ -41,15 +48,15 @@ window.addEventListener("devicemotion", deviceMotionHandler, false);
 
 var redraw = function()
 {
-    context.fillStyle = "black";
-    context.fillRect(0, 0, canvasDataHolder.size[0], canvasDataHolder.size[1]);
+    contextLeft.fillStyle = "black";
+    contextLeft.fillRect(0, 0, canvasDataHolder.size[0], canvasDataHolder.size[1]);
 
     var x = canvasDataHolder.middle[0] + spatialDataHolder.orientation[0];
-    var y = canvasDataHolder.middle[1] + spatialDataHolder.orientation[2];
+    var y = canvasDataHolder.middle[1] - spatialDataHolder.orientation[2];
 
-    context.fillStyle = "white";
-    context.fillRect(x - 5, y - 5, 10, 10);
+    contextLeft.fillStyle = "white";
+    contextLeft.fillRect(x - 5, y - 5, 10, 10);
 };
 
-var redrawHandle = window.setInterval(redraw, 100);
-resizeHandler(this);
+var redrawHandle = window.setInterval(redraw, 10);
+resizeHandler();
