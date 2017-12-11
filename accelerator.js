ae62e6678aa84bb9457002346a28adf63efd0c00
Tomasz Czarnecki <creo.one@gmail.com>
2017-12-11 19:21:12 +0100

    Accelerator work.

--- accelerator.js
+++ accelerator.js
@@ -2,13 +2,11 @@
 var Accelerator = function()
 {
     this.motion = new Vector3();
-    this.position = new Vector3();
+    this.strength = 5;
 };
 
 Accelerator.prototype.handleMotionEvent = function(e)
 {
-    var strength = 5;
-
-    this.motion = Vector3.div(Vector3.add(Vector3.mul(this.motion, Vector3.fromScalar(strength-1)), new Vector3(e.acceleration.y, e.acceleration.x, e.acceleration.z)), Vector3.fromScalar(strength + 0.2));
-    this.position = Vector3.add(this.position, this.motion);
+    var input = new Vector3(e.acceleration.y, e.acceleration.x, e.acceleration.z);
+    this.motion = Vector3.div(Vector3.add(Vector3.mul(this.motion, Vector3.fromScalar(this.strength-1)), input), Vector3.fromScalar(this.strength));
 };
--- main.js
+++ main.js
@@ -34,26 +34,30 @@ var resizeHandler = function(e)
 window.addEventListener("resize", resizeHandler, false);
 
 var accelerator = new Accelerator();
-window.addEventListener("devicemotion", function(e){ accelerator.handleMotionEvent(e); }, false);
+window.addEventListener("devicemotion", function(e){ accelerator.strength = timer.fps; accelerator.handleMotionEvent(e); }, false);
 
 var gyroscope = new Gyroscope();
 window.addEventListener("deviceorientation", function(e){ gyroscope.handleOrientationEvent(e); }, false);
 
 var redraw = function(t)
 {
-    context.fillStyle = "rgb(20, 20, 20)";
+    context.fillStyle = "rgb(10, 10, 10)";
     context.fillRect(0, 0, renderer.size.x * 2, renderer.size.y);
     
     renderer.clear();
 
-    var position = Vector2.add(renderer.half, Vector2.mul(new Vector2(-gyroscope.orientation.x, gyroscope.orientation.y), Vector2.fromScalar(400)));
-    renderer.uiCircle(position, 8, "rgba(255, 255, 255, 0.3)", 2);
+    var orientation = Vector2.add(renderer.half, Vector2.mul(new Vector2(-gyroscope.orientation.x, gyroscope.orientation.y), Vector2.fromScalar(400)));
+    renderer.uiCircle(orientation, 8, "rgba(255, 255, 255, 1.0)", 1);
 
-    var fontColor = "rgba(255, 255, 255, 0.7)";
-    var textX = Math.max(-renderer.ocularShift + 10, 10);
-    renderer.uiText("fps: " + Math.round(timer.fps), new Vector2(textX, 15), fontColor);
-    renderer.uiText("s: " + renderer.size.x * 2 + " " + renderer.size.y, new Vector2(textX, 25), fontColor);
-    renderer.uiText("o: " + round(gyroscope.orientation.x, -3) + " " + Math.round(gyroscope.orientation.y, -3), new Vector2(textX, 35), fontColor);
+    var position = Vector2.add(renderer.half, Vector2.mul(new Vector2(accelerator.motion.x, accelerator.motion.y), Vector2.fromScalar(100)));
+    renderer.uiCircle(position, 8, "rgba(255, 255, 255, 1.0)", 1);
+
+    var fontColor = "rgba(255, 255, 255, 1.0)";
+    var textX = Math.max(-renderer.ocularShift + 20, 20);
+    renderer.uiText("fps: " + Math.round(timer.fps), new Vector2(textX, 25), fontColor);
+    renderer.uiText("s: " + renderer.size.x * 2 + " " + renderer.size.y, new Vector2(textX, 35), fontColor);
+    renderer.uiText("o: " + round(gyroscope.orientation.x, -3) + " " + Math.round(gyroscope.orientation.y, -3), new Vector2(textX, 45), fontColor);
+    renderer.uiText("a: " + round(accelerator.motion.x, -3) + " " + Math.round(accelerator.motion.y, -3) + " " + Math.round(accelerator.motion.z, -3), new Vector2(textX, 55), fontColor);
 
     renderer.render(context);
     timer.tick();
